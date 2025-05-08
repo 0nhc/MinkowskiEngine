e105c77115508fda618e37978b17f4064b5872a8
0nhc <thefoxfoxfox@outlook.com>
2025-05-08 00:11:54 -0500

    fix compiling issus

--- setup.py
+++ setup.py
@@ -174,6 +174,7 @@ if sys.platform == "win32":
         CC_FLAGS += ["/sdl", "/permissive-"]
 else:
     CC_FLAGS += ["-fopenmp"]
+    NVCC_FLAGS += ["-Xcompiler", "-fopenmp"]
 
 if "darwin" in platform:
     CC_FLAGS += ["-stdlib=libc++", "-std=c++17"]
